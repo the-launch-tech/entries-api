ec53d62011e6d8043cfdc4e839bbfdeaba662c4f
The Launch <daniel@thelaunch.tech>
2019-12-14 10:21:29 -0500

    working api

--- Entry.php
+++ Entry.php
@@ -19,17 +19,9 @@ class Entry {
   }
 
   function create() {
-    $this->to_address = strip_tags($this->to_address);
-    $this->from_name = strip_tags($this->from_name);
-    $this->from_address = strip_tags($this->from_address);
-    $this->replyto_name = strip_tags($this->replyto_name);
-    $this->replyto_address = strip_tags($this->replyto_address);
-    $this->cc = strip_tags($this->cc);
-    $this->bcc = strip_tags($this->bcc);
-    $this->subject = strip_tags($this->subject);
-    $this->html = strip_tags($this->html, '<div><span><strong><b><i><p><a><br><li><ul><small>');
+    $status = false;
 
-    $query = $this->db->prepare("
+    $query = "
       INSERT INTO " . $this->table_name . " (
         to_address,
         from_name,
@@ -41,24 +33,40 @@ class Entry {
         subject,
         html
       )
-      VALUES (
-        '{$this->to_address}',
-        '{$this->from_name}',
-        '{$this->from_address}',
-        '{$this->replyto_name}',
-        '{$this->replyto_address}',
-        '{$this->cc}',
-        '{$this->bcc}',
-        '{$this->subject}',
-        '{$this->html}'
-      )
-    ");
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ";
 
-    if ($query->execute()) {
-      return true;
+    $stmt = $this->db->prepare($query);
+
+    $this->to_address = strip_tags($this->to_address);
+    $this->from_name = strip_tags($this->from_name);
+    $this->from_address = strip_tags($this->from_address);
+    $this->replyto_name = strip_tags($this->replyto_name);
+    $this->replyto_address = strip_tags($this->replyto_address);
+    $this->cc = strip_tags($this->cc);
+    $this->bcc = strip_tags($this->bcc);
+    $this->subject = strip_tags($this->subject);
+    $this->html = strip_tags($this->html, '<div><span><strong><b><i><p><a><br><li><ul><small>');
+
+    $stmt->bind_param("sssssssss",
+      $this->to_address,
+      $this->from_name,
+      $this->from_address,
+      $this->replyto_name,
+      $this->replyto_address,
+      $this->cc,
+      $this->bcc,
+      $this->subject,
+      $this->html,
+    );
+
+    if ($stmt->execute()) {
+      $status = true;
     }
 
-    return false;
+    $stmt->close();
+
+    return $status;
   }
 
 }
--- index.php
+++ index.php
@@ -1,25 +1,21 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header("Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require "Entry.php";
 
-if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
-  $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
-}
-
 try {
   if (strtoupper($_SERVER['REQUEST_METHOD']) === 'POST') {
 
     $db = new mysqli(
-      '127.0.0.1',
-      '<user',
-      '<pw>',
-      '<dbname>'
+      'localhost',
+      'mailer',
+      'mailer',
+      'mailer'
     );
 
     if ($db->connect_error) {
@@ -28,35 +24,33 @@ try {
 
     $Entry = new Entry($db);
 
-    $data = json_decode(file_get_contents("php://input"));
-
     if (
-      !empty($data->to_address) &&
-      !empty($data->from_name) &&
-      !empty($data->from_address) &&
-      !empty($data->html)
+      !empty($_POST['to_address']) &&
+      !empty($_POST['from_name']) &&
+      !empty($_POST['from_address']) &&
+      !empty($_POST['html'])
     ) {
 
-      $Entry->to_address = property_exists($data, 'to_address') ? $data->to_address : '';
-      $Entry->from_name = property_exists($data, 'from_name') ? $data->from_name : '';
-      $Entry->from_address = property_exists($data, 'from_address') ? $data->from_address : '';
-      $Entry->replyto_name = property_exists($data, 'replyto_name') ? $data->replyto_name : '';
-      $Entry->replyto_address = property_exists($data, 'replyto_address') ? $data->replyto_address : '';
-      $Entry->cc = property_exists($data, 'cc') ? $data->cc : '';
-      $Entry->bcc = property_exists($data, 'bcc') ? $data->bcc : '';
-      $Entry->subject = property_exists($data, 'subject') ? $data->subject : '';
-      $Entry->html = property_exists($data, 'html') ? $data->html : '';
+      $Entry->to_address = $_POST['to_address'];
+      $Entry->from_name = $_POST['from_name'];
+      $Entry->from_address = $_POST['from_address'];
+      $Entry->replyto_name = array_key_exists('replyto_name', $_POST) ? $_POST['replyto_name'] : '';
+      $Entry->replyto_address = array_key_exists('replyto_address', $_POST) ? $_POST['replyto_address'] : '';
+      $Entry->cc = array_key_exists('cc', $_POST) ? $_POST['cc'] : '';
+      $Entry->bcc = array_key_exists('bcc', $_POST) ? $_POST['bcc'] : '';
+      $Entry->subject = array_key_exists('subject', $_POST) ? $_POST['subject'] : '';
+      $Entry->html = $_POST['html'];
 
       if ($Entry->create()) {
         http_response_code(201);
-        echo json_encode(array("message" => "Entry was created."));
+        echo json_encode(array("message" => "Entry was created.", "Entry" => $Entry));
       } else {
         http_response_code(503);
-        echo json_encode(array("message" => "Unable to create Entry."));
+        echo json_encode(array("message" => "Unable to create Entry.", "Entry" => $Entry));
       }
     } else {
       http_response_code(400);
-      echo json_encode(array("message" => "Unable to create Entry. Data is incomplete."));
+      echo json_encode(array("message" => "Unable to create Entry. Data is incomplete.", "Entry" => $Entry));
     }
 
     $db->close();
